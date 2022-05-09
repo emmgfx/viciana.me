9caae57ff99afdb1c6cb3058a40aabaf6a26d760
Josep Viciana <emmgfx@gmail.com>
2022-05-09 15:40:13 +0200

    Improve articles responsive

--- components/Articles.js
+++ components/Articles.js
@@ -6,8 +6,8 @@ const Articles = ({ articles }) => {
     <section>
       <SectionTitle>03. Some of my articles</SectionTitle>
       <div className="h-10" />
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
-        {articles.slice(0, 4).map((article, index) => (
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {articles.slice(0, 4).map((article) => (
           <Article key={article.id} article={article} />
         ))}
       </div>
