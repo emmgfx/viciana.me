217f1448d9452df6caceadd9888e3a165f501c24
Josep Viciana <emmgfx@gmail.com>
2022-05-09 22:11:42 +0200

    Articles animation

--- components/Article.js
+++ components/Article.js
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
+import useIntersectionObserver from "@react-hook/intersection-observer";
+import classNames from "classnames";
+
 import IconArrow from "../public/images/icons/icon-arrow-right.svg";
 
-const Articles = ({ article }) => {
+const Article = ({ article }) => {
+  const [ref, setRef] = useState(null);
+  const [intersected, setIntersected] = useState(false);
+  const { isIntersecting } = useIntersectionObserver(ref, { threshold: 0.33 });
+
+  useEffect(() => {
+    if (intersected) return;
+    if (isIntersecting) setIntersected(true);
+  }, [intersected, isIntersecting]);
+
   return (
-    <article className="py-6 px-8 bg-[#102340]">
+    <article
+      ref={setRef}
+      className={classNames(
+        "py-6 px-8 bg-[#102340] transition-all",
+        !intersected && "opacity-0 translate-y-6",
+        intersected && "opacity-100 translate-y-0"
+      )}
+    >
       <h3 className="font-mono font-bold mb-4 line-clamp-2 leading-8">
         {article.title.rendered}
       </h3>
@@ -24,4 +44,4 @@ const Articles = ({ article }) => {
   );
 };
 
-export default Articles;
+export default Article;
