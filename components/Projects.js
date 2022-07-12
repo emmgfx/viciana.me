2343075ca1c784326659ae9114e57149a395cfd0
Josep Viciana <emmgfx@gmail.com>
2022-07-12 16:08:11 +0200

    Improve logos and scrollbars

--- components/Projects.js
+++ components/Projects.js
@@ -6,7 +6,13 @@ const Projects = ({ projects }) => {
     <section id="work">
       <SectionTitle>02. A few open source projects</SectionTitle>
       <div className="h-10" />
-      <div className="flex flex-nowrap gap-6 overflow-x-auto snap-x snap-mandatory md:grid-cols-3">
+      <div
+        className="
+        custom-scroll
+        flex flex-nowrap gap-6 overflow-x-auto snap-x snap-mandatory
+        md:grid md:grid-cols-3
+        pb-2"
+      >
         {projects.slice(0, 4).map((project) => (
           <Project key={project.id} project={project} />
         ))}
