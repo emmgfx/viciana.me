c3c5f8be316a4fb5f59a49409a142727850d2735
Josep Viciana <emmgfx@gmail.com>
2024-12-30 13:25:46 +0100

    Add address element

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,15 @@ export const Footer = () => {
   return (
     <footer className="text-sm mt-6 md:mt-8 border-t border-t-slate-700">
       <div className="text-center py-4 md:py-8 px-4 md:px-8 text-slate-400">
-        &copy; {date.getFullYear()}{" "}
-        <Link
-          href="/"
-          className="focus-visible:underline focus-visible:outline-none"
-        >
-          Josep Viciana
-        </Link>
+        &copy; {date.getFullYear()},{" "}
+        <address className="inline not-italic">
+          <Link
+            href="/"
+            className="focus-visible:underline focus-visible:outline-none hover:underline"
+          >
+            Josep Viciana
+          </Link>
+        </address>
       </div>
     </footer>
   );
