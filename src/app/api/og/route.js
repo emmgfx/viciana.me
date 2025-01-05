461d696efe694fc8bed2f088614efc4e068e5ba4
Josep Viciana <emmgfx@gmail.com>
2025-01-05 15:48:07 +0100

    Adjust sizes

--- src/app/api/og/route.js
+++ src/app/api/og/route.js
@@ -31,11 +31,11 @@ export async function GET(request) {
       >
         <div tw="absolute inset-0 p-12 flex flex-col bg-slate-900 text-slate-100">
           <div tw="flex">
-            <div tw="text-[64px] font-semibold grow">{title}</div>
+            <div tw="text-[100px] font-semibold grow">{title}</div>
             <div tw="flex"></div>
           </div>
           <div tw="grow" />
-          <div tw="text-[32px] flex">
+          <div tw="text-[46px] flex">
             {tags.map((tag) => (
               <div tw="mr-8" key={tag}>
                 {tag}
@@ -43,7 +43,7 @@ export async function GET(request) {
             ))}
           </div>
           <div tw="h-0.5 my-8 bg-slate-100" />
-          <div tw="text-[36px] items-center self-end flex">
+          <div tw="text-[48px] items-center self-end flex">
             <div>Josep Viciana</div>
             {/* <div tw="w-8"></div>
             <img
