7ac56a7aee1f8a0fc44090bd59e593a9a2ea4c14
Josep Viciana <emmgfx@gmail.com>
2025-08-26 07:38:33 +0200

    Robots

--- src/app/robots.js
+++ src/app/robots.js
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.viciana.me.com/sitemap.xml",
+    sitemap: "https://www.viciana.me/sitemap.xml",
   };
 }
