240a6c6ae4d5e0888e98f0b45bcd375ce57357a2
Josep Viciana <emmgfx@gmail.com>
2025-08-26 11:24:32 +0200

    Umami

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
-import "./globals.css";
-import "./code.css";
-
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+
+import "./globals.css";
+import "./code.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,12 @@ export default function RootLayout({ children }) {
       <body
         className={`flex flex-col min-h-screen bg-slate-900 text-slate-100 ${inter.className} has-[dialog[open]]:overflow-hidden`}
       >
+        <Script
+          defer
+          strategy="beforeInteractive"
+          src="https://cloud.umami.is/script.js"
+          data-website-id="94d6fa02-be4a-440f-80fc-47b129fbfbaa"
+        />
         <Header />
         <main className="mx-auto w-full max-w-5xl grow px-4 md:px-8">
           {children}
