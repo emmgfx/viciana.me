e1fd383e473f05b4368d82fc3c5a3eeb2319a6c1
Josep Viciana <emmgfx@gmail.com>
2025-08-26 08:30:12 +0200

    Post jsonld

--- src/app/articles/[slug]/page.js
+++ src/app/articles/[slug]/page.js
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import Script from "next/script";
 
 import { PostDetail } from "@/components/PostDetail";
 import { getPostBySlug, getPostSlugs } from "@/shared/api";
@@ -47,5 +48,55 @@ export default async function Article(props) {
   const params = await props.params;
   const post = getPostBySlug(params.slug);
   if (!post) notFound();
-  return <PostDetail post={post} />;
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting",
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id":
+        "https://www.viciana.me/articles/2025-08-23-use-polymorphism-to-ditch-legacybehavior-nextjs",
+    },
+    headline: post.data.title,
+    description: post.data.description,
+    image: "https://www.viciana.me/images/polymorphism-nextjs-cover.jpg",
+    author: {
+      "@type": "Person",
+      name: "Josep Viciana",
+      alternateName: "emmgfx",
+      description: "Software developer",
+      image: "https://www.viciana.me/avatar.jpeg",
+      sameAs: [
+        "https://www.viciana.me",
+        "https://www.emm-gfx.net",
+        "https://x.com/josep_viciana",
+        "https://www.linkedin.com/in/josep-viciana/",
+        "https://github.com/emmgfx",
+      ],
+    },
+    publisher: {
+      "@type": "Organization",
+      name: "viciana.me",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://www.viciana.me/avatar.jpeg",
+      },
+    },
+    datePublished: post.data.date,
+    dateModified: post.data.date,
+  };
+
+  return (
+    <>
+      <Script
+        id="post-schema"
+        type="application/ld+json"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd),
+        }}
+      />
+      <PostDetail post={post} />;
+    </>
+  );
 }
